83360f9b50c6a14371c1225c096f1fb931483863
Tiago <ufomobilegroup@gmail.com>
2019-01-07 15:02:07 -0500

    Reports visaul part front

--- js/app.js
+++ js/app.js
@@ -657,5 +657,78 @@ app.controller("DashboardController",function ($scope,$http) {
 // Reports Controller
 //----------------------------------------------------------------------------------------------------------------------
 app.controller("ReportsController",function ($scope,$http) {
+    
+    $scope.initRports = function () {
+        initializeStatsForTest();
+    };
+    
+    function initializeStatsForTest() {
+        var ctx = document.getElementById("pendingStats").getContext('2d');
+        var ctx2 = document.getElementById("timelineStats").getContext('2d');
+        data = {
+            datasets: [{
+                data: [10, 20, 30],
+                backgroundColor: ["#F2D633", "#4E7220","#ba332b"],
+            }],
+
+            // These labels appear in the legend and in the tooltips when hovering different arcs
+            labels: [
+                'En proceso',
+                'Completados',
+                'Caducados'
+            ]
+        };
+        // For a pie chart
+        var myPieChart = new Chart(ctx,{
+            type: 'pie',
+            data: data,
+            options: {}
+        });
+        var myChart2 = new Chart(ctx2, {
+            type: 'bar',
+            data: {
+                labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+                datasets: [{
+                    data: [12, 19, 3, 5, 2, 3],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                legend: {
+                    display: false
+                },
+                tooltips: {
+                    callbacks: {
+                        label: function(tooltipItem) {
+                            return tooltipItem.yLabel;
+                        }
+                    }
+                },
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero:true
+                        }
+                    }]
+                }
+            }
+        });
+    }
 
 });
